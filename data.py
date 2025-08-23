e0d9e3384b18d2ed040193f2e3839dfedb7f6580
sinjed <oooguuh@gmail.com>
2025-08-24 01:05:43 +0200

    added base repr class
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- data.py
+++ data.py
@@ -1,43 +1,50 @@
 from typing import Literal
+import json
+
+class Base:
+
+	def __repr__(self):
+		return json.dumps(self.__dict__, indent=4)
 
 class MovieDataBase:
 	movie_name: str
+	movie_formatted_name: str
 	movie_url: str
 	movie_poster_url: str
-	movie_release_date: str
+	# movie_release_date: str
 
-class WatchedDataActivity(MovieDataBase):
+class WatchedDataActivity(MovieDataBase, Base):
 	user_rating: str
 	user_comment: str | None
 
-class RewatchedDataActivity(MovieDataBase):
+class RewatchedDataActivity(MovieDataBase, Base):
 	user_rating: str
 	user_comment: str | None
 
-class RatedDataActivity(MovieDataBase):
+class RatedDataActivity(MovieDataBase, Base):
 	user_rating: str
 	user_comment: str | None
 
-class FollowedDataActivity:
+class FollowedDataActivity(Base):
 	user_followed_name: str
 	user_followed_pfp: str
 	user_followed_profile_url: str
 
 # Either review OR list
-class LikedReviewDataActivity(MovieDataBase):
+class LikedReviewDataActivity(MovieDataBase, Base):
 	reviewer_username: str
 	reviewer_profile_url: str
 	reviewer_pfp: str
 	reviewer_rating: str
 
-class LikedListDataActivity:
+class LikedListDataActivity(Base):
 	list_owner_username: str
 	list_owner_profile_url: str
 	list_owner_pfp: str
 	list_name: str
 	list_url: str
 
-class ListedDataActivity:
+class ListedDataActivity(Base):
 	list_name: str
 	list_url: str
 	film_amount: int
