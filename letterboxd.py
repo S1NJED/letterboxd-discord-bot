41028bad5774025c144ac212ffc2f04bac851b01
sinjed <oooguuh@gmail.com>
2025-08-24 03:46:16 +0200

    fix
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- letterboxd.py
+++ letterboxd.py
@@ -67,19 +67,22 @@ def getUserActivty(username: str) -> list[WatchedActivity | RewatchedActivity | 
             data: WatchedDataActivity | RewatchedActivity = WatchedDataActivity()
 
             if "-basic" in section.attrs.get("class"):
-                data.movie_name = section.select_one("a:nth-of-type(2)").text
+                data.movie_name = section.select_one("a:nth-of-type(2)").text.strip()
                 data.movie_formatted_name = section.select_one("a:nth-of-type(2)").attrs.get("href").split('/')[2]
                 data.movie_url = f"https://letterboxd.com/film/" + data.movie_formatted_name
                 data.movie_poster_url = getMoviePoster(data.movie_formatted_name)
                 data.user_rating = section.select_one("span.rating").text.strip()
                 data.user_comment = None
             else:
-                data.movie_name = section.select_one("div.body > header > span > h2 > a").text
+                data.movie_name = section.select_one("div.body > header > span > h2 > a").text.strip()
                 data.movie_formatted_name = section.select_one("div.body > header > span > h2 > a").attrs.get("href").split('/')[2]
                 data.movie_url = f"https://letterboxd.com/film/" + data.movie_formatted_name
                 data.movie_poster_url = getMoviePoster(data.movie_formatted_name)
                 data.user_rating = section.select_one("span.rating").text.strip()
                 data.user_comment = section.select_one("div.body > div.js-review > div > p").text
+
+            if f"{activity_type} and rated" in data.movie_name:
+                data.movie_name = data.movie_name.replace(f"{activity_type} and rated", "").strip()
 
         # if activity_type in ["watched", "rewatched", "rated", "liked_review"]:
             
