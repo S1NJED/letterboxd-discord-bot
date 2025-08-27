20bc89dd83b33c45081782f1c1a7a19215f8f2a7
sinjed <oooguuh@gmail.com>
2025-08-27 19:31:07 +0200

    removed uselss keys
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,16 +1,14 @@
 CREATE TABLE IF NOT EXISTS Users (
-    user_id TEXT PRIMARY KEY,
-    username TEXT,
-    deleted TINYINT DEFAULT 0,
+    username TEXT PRIMARY KEY,
     added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS Activities (
     activity_id TEXT PRIMARY KEY,
-    user_id TEXT,
+    username TEXT,
     added at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (username) REFERENCES Users(username)
 );
 
 CREATE TABLE IF NOT EXISTS Config (
