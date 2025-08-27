a09e282689d1f19d9b1d4d79f43786a293d32cc2
sinjed <oooguuh@gmail.com>
2025-08-27 19:32:33 +0200

    test cmd
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- src/cogs/dev.py
+++ src/cogs/dev.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands, Interaction
 import discord
 import os
+from utils import db_query
 
 
 class SyncCog(Cog):
@@ -16,6 +17,7 @@ class SyncCog(Cog):
 	async def sync(self, ctx: commands.Context):
 		synced = await self.bot.tree.sync()
 		await ctx.send(f"✅ Synced {len(synced)} command(s) for this guild")
+
 
 	@commands.command(name="reload")
 	@commands.is_owner()
@@ -31,6 +33,19 @@ class SyncCog(Cog):
 			await ctx.send(f"Sucessfully reloaded {len(cogs)} cogs ({', '.join(cogs)})")
 		else:
 			await ctx.send("Didnt reloaded any cogs ...")
+
+
+	@commands.command(name="test")
+	@commands.is_owner()
+	@commands.guild_only()
+	async def test(self, ctx: commands.Context):	
+		row = await db_query(
+			db_path=self.bot.db_path,
+			query="SELECT text_channel_id FROM Config",
+			fetch="one"
+		)
+		print(row, row)
+		print(row['text_channel_id'])
 
 async def setup(bot: Bot):
 	await bot.add_cog(SyncCog(bot))
